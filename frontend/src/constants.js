d5015113b3a30df19aa8f3238b9d48bda840bcc0
leonileo <leolittleprogrammer@gmail.com>
2024-09-05 02:08:05 +0300

    constants

---
+++ frontend/src/constants.js
@@ -0,0 +1,7 @@
+// constants.js
+// API URL'S
+export const BASE_URL = '';
+export const AUTH_URL = '/apiv1/auth';
+export const ADMIN_URL = '/apiv1/admin';
+export const CLIENT_URL = '/apiv1/client';
+export const TRAINER_URL = '/apiv1/trainer';
