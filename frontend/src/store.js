b87afbdeb127536419fd77f533c3be32e9770bab
leonileo <leolittleprogrammer@gmail.com>
2024-09-05 02:08:47 +0300

    Store

---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+// store.js
+// Import necessary modules
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/apiSlice";
+import authSliceReducer from "./slices/authSlice";
+
+const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: authSliceReducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
+});
+
+export default store;
