5f11a8ceec47959bc933af72a3d6be83701691f3
leonileo <leolittleprogrammer@gmail.com>
2024-09-06 04:19:31 +0300

    auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,62 @@
+// authMiddleware.js
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./asyncHandler.js');
+const Admin = require('../models/adminModel.js');
+const Trainer = require('../models/trainerModel.js');
+const Client = require('../models/clientModel.js');
+
+// Protect routes
+const protect = asyncHandler( async (req, res, next) => {
+    let token;
+
+    // Read the JWT from the cookie
+    token = req.cookies.jwt;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.admin = await Admin.findById(decoded.adminId).select('-password');
+            req.trainer = await Trainer.findById(decoded.trainerId).select('-password');
+            req.client = await Client.findById(decoded.clientId).select('-password');
+            next();
+        } catch(error){
+            res.status(401);
+            throw new Error ("Not authorized, token failed");
+        }
+    } else {
+        res.status(401);
+        throw new Error ("Not authorized, no token");
+    }
+
+} )
+
+// Client middleware
+const client = (req, res, next) => {
+    if (req.client && req.client.isClient) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error ("Not authorized as client!");
+    }
+}
+
+// Trainer middleware
+const trainer = (req, res, next) => {
+    if (req.trainer && req.trainer.isTrainer) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error ("Not authorized as trainer!");
+    }
+}
+// Admin middleware
+const admin = (req, res, next) => {
+    if (req.admin && req.admin.isAdmin) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error ("Not authorized as admin");
+    }
+}
+
+module.exports = { protect, admin, client, trainer };
