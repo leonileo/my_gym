a7c878d2181f0645dc2a56b3c091f708abfb01ed
leonileo <leolittleprogrammer@gmail.com>
2024-09-07 15:56:27 +0300

    Update: added encription, updated the schema for the trainer model

--- backend/models/trainerModel.js
+++ backend/models/trainerModel.js
@@ -2,9 +2,10 @@
 // trainerModel.js
 // Imort necessary modules
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const trainerSchema = new mongoose.Schema({
-    trainerId: {type: String, unique: true},
+    trainerId: {type: String, unique: true}, // format T-Id-0
     firstName: {type: String},
     fatherName: {type: String, unique: true},
     sex: {type: String},
@@ -13,14 +14,28 @@ const trainerSchema = new mongoose.Schema({
     verifiedTrainer: {type: Boolean},
     description: {type: String},
     serviceList: {type: Array},
-    email: {type: String},
-    phoneNo: {type: Number},
+    email: {type: String, unique: true},
+    phoneNo: {type: Number, unique: true},
+    password: {type: String},
     myWorkouts: {type: Array},
     isAccountFrozen: {type: Boolean, default: false},
     isAdmin: {type: Boolean, default: false},
     isTrainer: {type: Boolean, default: true},
     isClient: {type: Boolean, default: false},
 }, {timestamps: true})
+
+trainerSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+trainerSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    };
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
 
 const Trainer = mongoose.model('Trainer', trainerSchema);
 module.exports = Trainer
