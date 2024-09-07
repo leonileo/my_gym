6762f936e7c1a81b93bfeabfb6bcdae6d9409eb7
leonileo <leolittleprogrammer@gmail.com>
2024-09-07 15:49:27 +0300

    Update: added encription, update the schema for the client model

--- backend/models/clientModel.js
+++ backend/models/clientModel.js
@@ -2,21 +2,22 @@
 // clientModel.js
 // Imort necessary modules
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const clientSchema = new mongoose.Schema({    
     clientId: {type: String, unique: true},
     firstName: {type: String, required: true},
     fatherName: {type: String, required: true},
     sex: {type: String, required: true},
-    DOB: {type: Date, required: true},
+    DOB: {type: Date, required: true}, // format yyyy-mm-dd
     trainerId: {type: String, default: null},
-    picture: {type: String, required: true},
-    progressId: {type: String},
+    picture: {type: String},
+    progressId: {type: String, unique: true},
     weightBeforeTraining: {type: String},
     currentWeight: {type: String},
     plannedWeight: {type: String},
-    email: {type: String},
-    phoneNo: {type: Number},
+    email: {type: String, unique: true},
+    phoneNo: {type: Number, unique: true, default: null},
     password: {type: String},
     workoutsAssignedForMe: {type: Array},
     isAccountFrozen: {type: Boolean},
@@ -24,6 +25,19 @@ const clientSchema = new mongoose.Schema({
     isTrainer: {type: Boolean, default: false},
     isClient: {type: Boolean, default: true},
 }, {timestamps: true})
+
+clientSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+clientSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    };
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
 
 const Client = mongoose.model("Client", clientSchema);
 module.exports = Client
