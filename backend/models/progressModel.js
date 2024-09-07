5620f12081e147ec4802305eb39c1dfd34b212a5
leonileo <leolittleprogrammer@gmail.com>
2024-09-07 15:53:51 +0300

    Progress model schema

--- backend/models/progressModel.js
+++ backend/models/progressModel.js
@@ -4,11 +4,11 @@
 const mongoose = require('mongoose');
 
 const progressSchema = new mongoose.Schema({
-    date: {type: Date, required: true, unique: true},
-    clientId: {type: String, required: true},
-    clientPicture: {type: String, required: true},
-    weightInPicture: {type: String, required: true},
-    weightInNumber: {type: Number, required: true},
+    progressId: {type: String, required: true, unique: true},
+    clientId: {type: String, required: true, unique: true},
+    clientPicture: {type: String},
+    weightInPicture: {type: String},
+    weightInNumber: {type: Number},
 }, {timestamps: true});
 
 const Progress = mongoose.model('Progress', progressSchema);
