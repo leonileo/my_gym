d0f62a7375f714ec348d11c9da30409a0b39c32d
leonileo <leolittleprogrammer@gmail.com>
2024-09-10 20:38:49 +0300

    Update: updated schema for the progress model.

--- backend/models/progressModel.js
+++ backend/models/progressModel.js
@@ -6,10 +6,25 @@ const mongoose = require('mongoose');
 const progressSchema = new mongoose.Schema({
     progressId: {type: String, required: true, unique: true},
     clientId: {type: String, required: true, unique: true},
-    clientPicture: {type: String},
-    weightInPicture: {type: String},
-    weightInNumber: {type: Number},
+    progress: [{
+        workout: {type: mongoose.Schema.Types.ObjectId, ref: "Workout"},
+        clientPicture: {type: String},
+        weightInPicture: {type: String},
+        weightInNumber: {type: Number},
+        isWorkoutDone: {type: Boolean, default: false},
+        progressDate: {type: Date}
+    }]
 }, {timestamps: true});
 
 const Progress = mongoose.model('Progress', progressSchema);
 module.exports = Progress;
+
+// tariner add workout for client 
+// client id ena progress C-Id-2500 
+// grab progress id from client keza
+// workout-3 assign biyarglt ke progress lay yanen workout mechrsun check yedrgal keza
+// workoutid, clientid, 
+
+// find progress data with the client p id
+// then filter the progress section by today,
+// 
