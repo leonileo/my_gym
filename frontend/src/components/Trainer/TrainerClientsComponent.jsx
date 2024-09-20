126297e5e8ed5c552a1149224e9bf950f8dec02e
leonileo <leolittleprogrammer@gmail.com>
2024-09-20 05:17:10 +0300

    Update: updated the ui component.

--- frontend/src/components/Trainer/TrainerClientsComponent.jsx
+++ frontend/src/components/Trainer/TrainerClientsComponent.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import {AgeFromDateString} from 'age-calculator';
+import { AgeFromDateString } from 'age-calculator';
 import { AiOutlineMessage } from "react-icons/ai";
-import { Link } from 'react-router-dom';
+import gymImg from '../../assets/images/gymImg.jpg'
 
-const TrainerClientsComponent = ({picture, name, DOB, email, weightBeforeTraining, currentWeight, plannedWeight, id, setCurrId, currId }) => {
+const TrainerClientsComponent = ({ setClient, client, id, setCurrId, currId }) => {
   return (
   <div className='bg-white border rounded p-5 xl:max-h-[35vh] h-full grid gap-3'>
     <div className="top flex justify-between h-fit mb-5">
       <div className="left flex items-start gap-3 rounded-full">
         <div className="img w-[70px] h-[70px] rounded-full"
           style={{
-            backgroundImage: `url(${picture})`,
+            backgroundImage: `url(${client.picture ? client.picture : gymImg})`,
             backgroundRepeat: "no-repeat",
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         ></div>
         <div className="txt">
-          <p className='font-semibold md:text-2xl text-xl'>{name}</p>
+          <p className='font-semibold md:text-2xl text-xl'>{client.name}</p>
           <div className="flex gap-2 text-gray-500 items-start">
-            <p>{new AgeFromDateString(new Date(DOB)).age} Years</p>
-            <p>{email}</p>
+            <p>{new AgeFromDateString(new Date(client.DOB.substring(0, 10))).age} Years old</p>
+            <p>{client.email}</p>
           </div>
         </div>
       </div>
@@ -34,9 +34,22 @@ const TrainerClientsComponent = ({picture, name, DOB, email, weightBeforeTrainin
         {id === currId &&
         <div className="actmodal absolute bg-gray-50 border right-0 rounded w-[200px]">
           <button onClick={() => setCurrId('')} className='w-full flex gap-2 items-center hover:bg-gray-100 hover:text-teal-500 transition-all p-2'>Send a message <AiOutlineMessage /></button>
-          <Link to='/trainer/dashboard'>
-          <button className='w-full flex gap-2 items-center hover:bg-gray-100 hover:text-teal-500 transition-all p-2'>View details</button>
-          </Link>
+          <button onClick={() => { setClient(
+            client.clientId, 
+            client.firstName, 
+            client.fatherName, 
+            client.sex, 
+            client.DOB, 
+            client.picture, 
+            client.progressId, 
+            client.weightBeforeTraining, 
+            client.currentWeight, 
+            client.plannedWeight, 
+            client.email, 
+            client.phoneNo, 
+            client.workoutsAssignedForMe, 
+            client.isAccountFrozen
+          ); setCurrId('')}} className='w-full flex gap-2 items-center hover:bg-gray-100 hover:text-teal-500 transition-all p-2'>View details</button>
         </div>
         }
       </div>
@@ -46,19 +59,19 @@ const TrainerClientsComponent = ({picture, name, DOB, email, weightBeforeTrainin
       <div className="wbox flex justify-center text-center px-2 text-gray-500 border-x">
         <div>
           <p className='capitalize'><span className='xl:inline-block hidden'> Weight</span> before training</p>
-          <p className='text-gray-600 text-2xl font-semibold italic'>{weightBeforeTraining}</p>
+          <p className='text-gray-600 text-2xl font-semibold italic'>{client.weightBeforeTraining}</p>
         </div>
       </div>
       <div className="wbox flex justify-center text-center px-2 text-gray-500 border-x">
         <div>
           <p className='capitalize'>Current weight</p>
-          <p className='text-gray-600 text-2xl font-semibold italic'>{currentWeight}</p>
+          <p className='text-gray-600 text-2xl font-semibold italic'>{client.currentWeight}</p>
         </div>
       </div>
       <div className="wbox flex justify-center text-center px-2 text-gray-500 border-x">
         <div>
           <p className='capitalize'>Goal weight</p>
-          <p className='text-gray-600 text-2xl font-semibold italic'>{plannedWeight}</p>
+          <p className='text-gray-600 text-2xl font-semibold italic'>{client.plannedWeight}</p>
         </div>
       </div>
     </div>
