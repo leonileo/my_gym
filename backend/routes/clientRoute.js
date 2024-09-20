79f6c328876ba424e416842a1d12e044d75a2925
leonileo <leolittleprogrammer@gmail.com>
2024-09-20 05:14:08 +0300

    Update: added the updated secured route for client account.

--- backend/routes/clientRoute.js
+++ backend/routes/clientRoute.js
@@ -2,21 +2,23 @@
 // clientRoute.js
 // Import necessary modules
 const express = require('express');
-const { protect, client } = require('../middleware/authMiddleware.js');
+const { protectClient, client } = require('../middleware/authMiddleware.js');
 const { dashboard, getWorkouts, updateWorkout, addTrainer, getChats, getSpecificChat, addChat, getProfile, updateProfile } = require('../controllers/clientController.js');
 
 const router = express.Router();
 
-router.get('/dashboard', protect, client, dashboard);
-router.get('/workout', protect, client, getWorkouts);
-router.get('/workout/:id', protect, client, updateWorkout);
-router.post('/addtrainer', protect, client, addTrainer);
-router.get('/chat', protect, client, getChats);
+router.get('/dashboard', protectClient, client, dashboard);
+router.get('/workout', protectClient, client, getWorkouts);
+router.put('/workout/:id',
+    //  protectClient, client, 
+     updateWorkout);
+router.post('/addtrainer', protectClient, client, addTrainer);
+router.get('/chat', protectClient, client, getChats);
 router.route('/chat/:id')
-.get(protect, client, getSpecificChat)
-.post(protect, client, addChat)
+.get(protectClient, client, getSpecificChat)
+.post(protectClient, client, addChat)
 router.route('/profile')
-.get(protect, client, getProfile)
-.put(protect, client, updateProfile)
+.get(protectClient, client, getProfile)
+.put(protectClient, client, updateProfile)
 
 module.exports = router;
