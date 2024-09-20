52073cce4b5e35d34ba311bb545a6ada60475b83
leonileo <leolittleprogrammer@gmail.com>
2024-09-20 05:11:56 +0300

    Workout model

--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -4,14 +4,14 @@
 const mongoose = require('mongoose')
 
 const workoutSchema = new mongoose.Schema({
-    trainerId: {type: String, required: true, unique: true},
-    workoutName: {type: String, required: true},
+    trainerId: {type: String, required: true},
+    workoutName: {type: String, required: true, unique: true},
     workoutCategory: {type: String, required: true},
-    workoutDescription: {type: String, required: true},
+    workoutDescription: {type: String, required: true, unique: true},
     workoutPicture: {type: String},
     workoutVideoLink: {type: String},
     workoutDuration: {type: String, required: true},
-    workoutSteps: {type: String, required: true},
+    workoutSteps: {type: String},
     equipmentRequired: {type: Boolean, required: true},
     equipmentList: {type: Array}
 }, {timestamps: true})
