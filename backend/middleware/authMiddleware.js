26cc347ffc1f70b3ff0aa6c0a3cbd977af5abaea
leonileo <leolittleprogrammer@gmail.com>
2024-09-20 04:58:04 +0300

    Update: updated the jwt cookie assigning method for all 3 accounts separately.

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,8 +5,9 @@ const Admin = require('../models/adminModel.js');
 const Trainer = require('../models/trainerModel.js');
 const Client = require('../models/clientModel.js');
 
-// Protect routes
-const protect = asyncHandler( async (req, res, next) => {
+
+// Protected admin routes
+const protectAdmin = asyncHandler( async (req, res, next) => {
     let token;
 
     // Read the JWT from the cookie
@@ -15,9 +16,56 @@ const protect = asyncHandler( async (req, res, next) => {
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.admin = await Admin.findById(decoded.adminId).select('-password');
-            req.trainer = await Trainer.findById(decoded.trainerId).select('-password');
+            req.admin = await Admin.findById(decoded.adminId).select('-password')
+
+            next();
+        } catch(error){
+            res.status(401);
+            throw new Error ("Not authorized, token failed");
+        }
+    } else {
+        res.status(401);
+        throw new Error ("Not authorized, no token");
+    }
+
+} )
+
+// Protected trainer routes
+const protectTrainer = asyncHandler( async (req, res, next) => {
+    let token;
+
+    // Read the JWT from the cookie
+    token = req.cookies.jwt;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.trainer = await Trainer.findById(decoded.trainerId).select('-password')
+
+            next();
+        } catch(error){
+            res.status(401);
+            throw new Error ("Not authorized, token failed");
+        }
+    } else {
+        res.status(401);
+        throw new Error ("Not authorized, no token");
+    }
+
+} )
+
+// Protected client routes
+const protectClient = asyncHandler( async (req, res, next) => {
+    let token;
+
+    // Read the JWT from the cookie
+    token = req.cookies.jwt;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.client = await Client.findById(decoded.clientId).select('-password');
+
             next();
         } catch(error){
             res.status(401);
@@ -59,4 +107,4 @@ const admin = (req, res, next) => {
     }
 }
 
-module.exports = { protect, admin, client, trainer };
+module.exports = { protectAdmin, protectTrainer, protectClient, admin, client, trainer };
