8d30cb07e5f21744f97847b121e6eee9bf24d04a
Hana-Developer <hanatadesse2023@gmail.com>
2024-09-25 03:02:02 +0300

    Update: removed the chat routes

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -3,19 +3,20 @@
 // Import necessary modules
 const express = require('express');
 const { protectAdmin, admin } = require('../middleware/authMiddleware.js');
-const { dashboard, getClients, getTrainers, getChat, addChat, getProfile, updateProfile } = require('../controllers/adminController.js');
+const { dashboard, getClients, getTrainers, getProfile, updateProfile, suspendClientAccount, suspendTrainerAccount, activateClientAccount, activateTrainerAccount } = require('../controllers/adminController.js');
 
 const router = express.Router();
 
 router.get('/dashboard', protectAdmin, admin, dashboard)
 router.get('/clients', protectAdmin, admin, getClients)
 router.get('/trainers', protectAdmin, admin, getTrainers)
-router.get('/chat', protectAdmin, admin, getChat)
-router.route('/chat/:id')
-.get(protectAdmin, admin, getChat)
-.post(protectAdmin, admin, addChat)
 router.route('/profile')
 .get( protectAdmin, admin, getProfile)
 .put( protectAdmin, admin, updateProfile)
+router.put('/suspend-client-account', protectAdmin, admin, suspendClientAccount)
+router.put('/activate-client-account', protectAdmin, admin, activateClientAccount);
+
+router.put('/suspend-trainer-account', protectAdmin, admin, suspendTrainerAccount)
+router.put('/activate-trainer-account', protectAdmin, admin, activateTrainerAccount);
 
 module.exports = router;
