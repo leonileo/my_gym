0fd8e67a761ad6424a0d7a90ac1d8060f869f869
Hana-Developer <hanatadesse2023@gmail.com>
2024-09-25 03:07:17 +0300

    Update: integrated the component with the backend

--- frontend/src/components/Admin/AdminTrainersComponent.jsx
+++ frontend/src/components/Admin/AdminTrainersComponent.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { FaEye } from 'react-icons/fa6';
 import { MdNoAccounts } from 'react-icons/md';
-import gymImg from '../../assets/images/gymImg.jpg'
 
-const AdminTrainersComponent = ({ previewHandler, deleteHandler, trainers }) => {
+const AdminTrainersComponent = ({ previewHandler, deleteHandler, activateHandler, trainers }) => {
   return (
-    <table className='w-full overflow-auto'>
+    <table>
       <thead>
         <tr>
           <td className='text-nowrap pr-2'>N<u>o</u></td>
@@ -21,20 +20,25 @@ const AdminTrainersComponent = ({ previewHandler, deleteHandler, trainers }) => 
       <tbody>
         {trainers.map((trainer, id) => (
           <tr key={id}>
-            <td className='py-5 text-nowrap text-gray-500 pr-2'>{id + 1}</td>
-            <td className='py-5 text-nowrap text-gray-500 pr-2'>{trainer.trainerId}</td>
-            <td className='py-5 text-nowrap text-gray-500 pr-2'>{trainer.firstName}</td>
-            <td className='py-5 text-nowrap text-gray-500 pr-2'>{trainer.fatherName}</td>
-            <td className='py-5 text-nowrap text-gray-500 pr-2'>{trainer.sex}</td>
-            <td className='py-5 text-nowrap text-gray-500 pr-2'>{trainer.email ? trainer.email : trainer.phoneNo}</td>
-            <td className='py-5 text-nowrap text-gray-500 pr-2'>{trainer.description}</td>
+            <td className={`p-5 text-nowrap text-gray-500 `}>{id + 1}</td>
+            <td className={`p-5 text-nowrap text-gray-500 ${trainer.isAccountFrozen && "line-through"} `}>{trainer.trainerId}</td>
+            <td className={`p-5 text-nowrap text-gray-500 ${trainer.isAccountFrozen && "line-through"} `}>{trainer.firstName}</td>
+            <td className={`p-5 text-nowrap text-gray-500 ${trainer.isAccountFrozen && "line-through"} `}>{trainer.fatherName}</td>
+            <td className={`p-5 text-nowrap text-gray-500 ${trainer.isAccountFrozen && "line-through"} `}>{trainer.sex}</td>
+            <td className={`p-5 text-nowrap text-gray-500 ${trainer.isAccountFrozen && "line-through"} `}>{trainer.email ? trainer.email : trainer.phoneNo}</td>
+            <td className={`p-5 text-nowrap text-gray-500 ${trainer.isAccountFrozen && "line-through"} `}>{trainer.description ? trainer.description : "Not available"}</td>
             <td className='py-5 text-nowrap text-gray-500'>
               <div className='flex gap-2 items-center'>
                 <button onClick={() => previewHandler(
-                  trainer.trainerId, `${trainer.firstName} ${trainer.fatherName}`, trainer.sex, trainer.clients, gymImg, trainer.verifiedTrainer, trainer.description, trainer.serviceList, trainer.email, trainer.phoneNo, trainer.myWorkouts, trainer.accountCreationDate
+                  trainer.trainerId, `${trainer.firstName} ${trainer.fatherName}`, trainer.sex, trainer.clients.length, trainer.picture, trainer.verifiedTrainer, trainer.description, trainer.serviceList.length, trainer.email, trainer.phoneNo, trainer.myWorkouts.length, trainer.createdAt
                   )}
                   className='px-2 border border-blue-500 rounded text-blue-500 hover:bg-blue-500 hover:text-white transition-all flex gap-2 items-center'><FaEye/> Preview</button>
-                <button onClick={() => deleteHandler(gymImg, `${trainer.firstName} ${trainer.fatherName}`, trainer.email, trainer.phoneNo, trainer.progressId, trainer.clientId, trainer.sex, trainer.DOB, trainer.accountStatus, trainer.accountCreationDate, trainer.trainerPicture, trainer.trainerName, trainer.trainerId)} className='px-2 border border-red-500 rounded text-red-500 hover:bg-red-500 hover:text-white transition-all flex gap-2 items-center'><MdNoAccounts/> Suspend</button>
+                {trainer.isAccountFrozen 
+                  ?
+                  <button onClick={() => activateHandler(`${trainer.firstName} ${trainer.fatherName}`, trainer.trainerId)} className='px-2 border border-green-500 rounded text-green-500 hover:bg-green-500 hover:text-white transition-all flex gap-2 items-center'><MdNoAccounts/> Activate</button>
+                  :
+                  <button onClick={() => deleteHandler(`${trainer.firstName} ${trainer.fatherName}`, trainer.trainerId)} className='px-2 border border-red-500 rounded text-red-500 hover:bg-red-500 hover:text-white transition-all flex gap-2 items-center'><MdNoAccounts/> Suspend</button>
+                }
               </div>
             </td>
           </tr>
