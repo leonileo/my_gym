4cad76fb858b3b967ce217de67fe939e73e070b3
Hana-Developer <hanatadesse2023@gmail.com>
2024-09-25 03:17:35 +0300

    Update: updated the admin dashboard screen.

--- frontend/src/screens/Admin/AdminDashboard.jsx
+++ frontend/src/screens/Admin/AdminDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import AdminSideNavComponent from '../../components/Admin/AdminSideNavComponent'
-import { FaEye, FaPeopleGroup } from 'react-icons/fa6';
-import { MdGroupWork, MdNoAccounts } from 'react-icons/md';
+import { FaPeopleGroup } from 'react-icons/fa6';
+import { MdGroupWork } from 'react-icons/md';
 import { CgGym, CgWorkAlt } from 'react-icons/cg';
 import { DayPicker } from 'react-day-picker';
 import gymImg from '../../assets/images/gymImg.jpg'
@@ -11,36 +11,47 @@ import { Link } from 'react-router-dom';
 import { BsClipboard2Data } from "react-icons/bs";
 import { MdFiberNew } from "react-icons/md";
 import CountUp from 'react-countup';
+import { useAdashboardQuery } from '../../slices/adminApiSlice';
+import { AgeFromDateString } from 'age-calculator';
+import { format } from 'timeago.js'
+import { IoRefresh } from 'react-icons/io5';
+import { TbFileSad } from 'react-icons/tb';
+import { Spinner } from 'flowbite-react';
+import { useSelector } from 'react-redux';
 
 const AdminDashboard = () => {
   // states
   const [clpsd, setClpsd] = useState(false);
-  const [preview, setPreview] = useState();
-  const [deleteClient, setDeleteClient] = useState();
+  const {data: adminData, isLoading, error} = useAdashboardQuery();
 
+  const name = useSelector(state => state.auth).userInfo.name
+  
   const cards = [
     {
       title: "All clients",
-      number: 120,
+      number: isLoading ? "" : error ? "error" : adminData.clientsLength,
       icon: <FaPeopleGroup className='w-full h-full' />
     },
     {
-      title: "All clients",
-      number: 56,
+      title: "All trainers",
+      number: isLoading ? "" : error ? "error" : adminData.trainers,
       icon: <MdGroupWork className='w-full h-full' />
     },
     {
-      title: "My services",
-      number: 7,
+      title: "All services",
+      number: isLoading ? "" : error ? "error" : adminData.services,
       icon: <CgWorkAlt className='w-full h-full' />
     },
     {
-      title: "My workouts",
-      number: 35,
+      title: "All workouts",
+      number: isLoading ? "" : error ? "error" : adminData.workoutsLength,
       icon: <CgGym className='w-full h-full' />
     },
   ];
 
+  const clients = isLoading ? [] : error ? []: adminData.clients
+  const workouts = isLoading ? [] : error ? []: adminData.workouts
+  
   // dummy data
   // available
   // const workouts = [ 
@@ -166,7 +177,7 @@ const AdminDashboard = () => {
   //   },
   // ]
   // not available
-  const workouts = null;
+  // const workouts = null;
 
   // available
   // const clients = [
@@ -253,8 +264,7 @@ const AdminDashboard = () => {
   //   },
   // ]
   // not available
-  const clients = null;
-
+  // const clients = null;
   // dummy data
 
   return (
@@ -269,7 +279,7 @@ const AdminDashboard = () => {
               <div className="p-5 w-full">
                 <div className="contents w-full">
                   <div className="welcome my-5">
-                    <p>Hi {'name'}</p>
+                    <p>Hi <span className='font-semibold'>{name}</span></p>
                     <h2 className='md:text-3xl'>Welcome to My gym!</h2>
                   </div>
                   {/* status cards */}
@@ -279,7 +289,12 @@ const AdminDashboard = () => {
                       <div className='w-full group p-5 py-6 rounded-lg bg-gradient-to-r from-teal-400 to-teal-500 flex gap-4 justify-between'>
                         <div className="txt">
                           <h2 className='text-white text-xl text-nowrap'>{card.title}</h2>
-                          <p className='text-white xl:text-4xl'> <CountUp start={0} end={card.number ? card.number : 0} duration={6} /> </p>
+                          <p className={`text-white ${isLoading ||error ? "text-xl" : "xl:text-4xl"}`}> 
+                            { isLoading ? <Spinner />
+                            : error ? "An error occured"
+                            : <CountUp start={0} end={card.number ? card.number : 0} duration={6} /> 
+                            }
+                          </p>
                           </div>
                         <div className="icon group-hover:rotate-[360deg] duration-500 transition-all p-3 bg-teal-50 border rounded-full">
                           {card.icon}
@@ -291,61 +306,85 @@ const AdminDashboard = () => {
                     {/* left */}
                     <div className="left w-full h-full">
                       <div className="w-full h-full">
-                        {workouts ? 
-                        <Swiper
-                        slidesPerView={1}
-                        spaceBetween={30}
-                        freeMode={true}
-                        pagination={{
-                          clickable: true,
-                        }}
-                        modules={[Pagination]}
-                        className={`w-full md:hidden block`}
-                        >
-                      {/* loop through created workouts */}
-                      {workouts.map(created => (
-                        <SwiperSlide className='h-full w-full mb-7'>
-                        <div 
-                          style={{
-                            backgroundImage: `linear-gradient(45deg, rgba(255, 255, 255, .1), rgba(6, 148, 162, 1)), url(${gymImg})`,
-                            backgroundRepeat: "no-repeat",
-                            backgroundSize: "cover",
-                            backgroundPosition: "center",
-                          }}
-                          className='p-4 img rounded w-full mb-3 xl:h-[26vh] h-[35vh] relative'>
-                          <div className="trainer-box absolute top-0 right-0 xl:w-[40%] w-full xl:min-h-[50%] h-[70%] bg-gradient-to-r from-[rgba(255,255,255,0.2)] to-white flex justify-end py-5 p-2 rounded-l-full">
-                            <div className='space-y-2 text-center text-gray-700'>
-                              <p className='font-semibold'>Workout created <span className='text-gray-700'> 1 hour ago </span></p>
-                              <div className="flex justify-center">
-                                <div className='trainer-img flex justify-center items-center w-[70px] h-[70px] rounded-full'
-                                style={{
-                                  backgroundImage: `linear-gradient(45deg, rgba(255, 255, 255, .1), rgba(6, 148, 162, 1)), url(${gymImg})`,
-                                  backgroundRepeat: "no-repeat",
-                                  backgroundSize: "cover",
-                                  backgroundPosition: "center",
-                                }}
-                                ></div>
+                        { isLoading ? 
+                          <div className='flex bg-white rounded-md justify-center items-center w-full h-full transition-all'>
+                            <div className='flex gap-2 items-center xl:text-[20px] transition-all'>
+                              <p className='text-teal-500'>Loading...</p>
+                              <Spinner className='xl:w-[30px] xl:h-[30px]' />
+                            </div>
+                          </div>
+                          : error ? 
+                          <div className='flex bg-white rounded-md justify-center items-center w-full h-full transition-all'>
+                            <div className='flex gap-2 items-center xl:text-[20px] transition-all'>
+                              <div>
+                                <div className='flex items-center gap-4 text-center'>
+                                  <p className='text-teal-500'>An error has occured. please refresh the page and try again.</p>
+                                  <TbFileSad className='text-teal-500'/>
+                                </div>
+                                <div className='flex items-center justify-center gap-4 text-teal-400 animate-pulse'>
+                                  <button onClick={() => window.location.reload()} className='text-center flex gap-2 items-center'>Refresh</button>
+                                  <IoRefresh />
+                                </div>
                               </div>
-                              <p>Trainer name</p>
                             </div>
                           </div>
-                          <div className="flex items-end h-full">
-                            <p className='bg-[rgba(255,255,255,0.8)] w-fit p-2 px-5 rounded-full capitalize'>{created.workoutName}</p>
-                          </div>
-                        </div>
-                      </SwiperSlide>
-                      ))}
-                      </Swiper>
-                      : (
-                        <div className='rounded bg-white w-full h-full flex justify-center items-center my-5 xl:m-0'>
-                          <div>
-                            <div className="flex justify-center group">
-                            <MdFiberNew className='text-5xl group-hover:text-teal-500 transition-all'/> 
+                        : ( 
+                        workouts.length > 0 ? 
+                          <Swiper
+                          slidesPerView={1}
+                          spaceBetween={30}
+                          freeMode={true}
+                          pagination={{
+                            clickable: true,
+                          }}
+                          modules={[Pagination]}
+                          className={`w-full md:hidden block`}
+                          >
+                        {/* loop through created workouts */}
+                        {workouts.map(created => (
+                          <SwiperSlide className='h-full w-full mb-7'>
+                          <div 
+                            style={{
+                              backgroundImage: `linear-gradient(45deg, rgba(255, 255, 255, .1), rgba(6, 148, 162, 1)), url(${created.workoutPicture ? created.workoutPicture : gymImg})`,
+                              backgroundRepeat: "no-repeat",
+                              backgroundSize: "cover",
+                              backgroundPosition: "center",
+                            }}
+                            className='p-4 img rounded w-full mb-3 xl:h-[26vh] h-[35vh] relative'>
+                            <div className="trainer-box absolute top-0 right-0 xl:w-[40%] w-full xl:min-h-[50%] h-[70%] bg-gradient-to-r from-[rgba(255,255,255,0.2)] to-white flex justify-end py-5 p-2 rounded-l-full">
+                              <div className='space-y-2 text-center text-gray-700'>
+                                <p className='font-semibold'>Workout created <span className='text-gray-700'> {format(created.createdAt.substring(0,10))} </span></p>
+                                <div className="flex justify-center">
+                                  <div className='trainer-img flex justify-center items-center w-[70px] h-[70px] rounded-full'
+                                  style={{
+                                    backgroundImage: `linear-gradient(45deg, rgba(255, 255, 255, .1), rgba(6, 148, 162, 1)), url(${ created.workoutPicture ? created.workoutPicture : gymImg})`,
+                                    backgroundRepeat: "no-repeat",
+                                    backgroundSize: "cover",
+                                    backgroundPosition: "center",
+                                  }}
+                                  ></div>
+                                </div>
+                                <p>Trainer name</p>
+                              </div>
                             </div>
-                            <p className='my-2 text-center'>Newly created workouts will be displayed here.</p>
+                            <div className="flex items-end h-full">
+                              <p className='bg-[rgba(255,255,255,0.8)] w-fit p-2 px-5 rounded-full capitalize'>{created.workoutName}</p>
+                            </div>
                           </div>
-                        </div>
-                      )
+                        </SwiperSlide>
+                        ))}
+                        </Swiper>
+                        : (
+                          <div className='rounded bg-white w-full h-full flex justify-center items-center my-5 xl:m-0'>
+                            <div>
+                              <div className="flex justify-center group">
+                              <MdFiberNew className='text-5xl group-hover:text-teal-500 transition-all'/> 
+                              </div>
+                              <p className='my-2 text-center'>Newly created workouts will be displayed here.</p>
+                            </div>
+                          </div>
+                          )
+                        )
                         }
                       </div>
                     </div>
@@ -368,14 +407,38 @@ const AdminDashboard = () => {
                       <button className='p-2 rounded px-5 border bg-white gray-50 border-gray-300'>View all</button>
                       </Link>
                     </div>
+                    {/* clients list */}
                     <div className={`table my-2 px-5  ${ clients ? "bg-gray-50" : 'bg-white'} rounded w-full h-[35vh]`}>
                       {/* loop through clients list */}
-                      {clients !== null ?
-                        clients.map(client => (
-                          <>
+                      {isLoading ? 
+                          <div className='flex justify-center items-center w-full h-full transition-all'>
+                            <div className='flex gap-2 items-center xl:text-[20px] transition-all'>
+                              <p className='text-teal-500'>Loading...</p>
+                              <Spinner className='xl:w-[30px] xl:h-[30px]' />
+                            </div>
+                          </div>
+                          : error ? 
+                          <div className='flex justify-center items-center w-full h-full transition-all'>
+                          <div className='flex gap-2 items-center xl:text-[20px] transition-all'>
+                            <div>
+                              <div className='flex items-center gap-4 text-center'>
+                                <p className='text-teal-500'>An error has occured. please refresh the page and try again.</p>
+                                <TbFileSad className='text-teal-500'/>
+                              </div>
+                              <div className='flex items-center justify-center gap-4 text-teal-400 animate-pulse'>
+                                <button onClick={() => window.location.reload()} className='text-center flex gap-2 items-center'>Refresh</button>
+                                <IoRefresh />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      : clients.length > 0 ?
+                      <>
+                        <>
                           <table className='w-full'>
                             <thead>
                               <tr>
+                                <td className='text-nowrap pr-2'>N <u>O</u></td>
                                 <td className='text-nowrap pr-2'>Client id</td>
                                 <td className='text-nowrap pr-2'>First name</td>
                                 <td className='text-nowrap pr-2'>Father name</td>
@@ -383,33 +446,28 @@ const AdminDashboard = () => {
                                 <td className='text-nowrap pr-2'>Sex</td>
                                 <td className='text-nowrap pr-2'>Email</td>
                                 <td className='text-nowrap pr-2'>Trainer's id</td>
-                                <td>Actions</td>
                               </tr>
                             </thead>
                             <tbody>
+                            {clients.map((client, no) => (
                               <tr>
+                                <td className='py-5 text-nowrap text-gray-500 pr-2'>{no + 1}</td>
                                 <td className='py-5 text-nowrap text-gray-500 pr-2'>{client.clientId}</td>
                                 <td className='py-5 text-nowrap text-gray-500 pr-2'>{client.firstName}</td>
                                 <td className='py-5 text-nowrap text-gray-500 pr-2'>{client.fatherName}</td>
-                                <td className='py-5 text-nowrap text-gray-500 pr-2'>{client.DOB}</td>
+                                <td className='py-5 text-nowrap text-gray-500 pr-2'>{client.DOB ? `${client.DOB.substring(0, 10)} - ${new AgeFromDateString(new Date(client.DOB.substring(0, 10))).age} yrs old` : "Not available"}</td>
                                 <td className='py-5 text-nowrap text-gray-500 pr-2'>{client.sex}</td>
                                 <td className='py-5 text-nowrap text-gray-500 pr-2'>{client.email}</td>
-                                <td className='py-5 text-nowrap text-gray-500 pr-2'>{client.trainerId}</td>
-                                <td className='py-5 text-nowrap text-gray-500'>
-                                  <div className='flex gap-2 items-center'>
-                                    <button onClick={() => setPreview} className='px-2 border border-blue-500 rounded text-blue-500 hover:bg-blue-500 hover:text-white transition-all flex gap-2 items-center'><FaEye/> Preview</button>
-                                    <button onClick={() => setDeleteClient} className='px-2 border border-red-500 rounded text-red-500 hover:bg-red-500 hover:text-white transition-all flex gap-2 items-center'><MdNoAccounts/> Suspend</button>
-                                  </div>
-                                </td>
+                                <td className='py-5 text-nowrap text-gray-500 pr-2'>{client.trainerId ? client.trainerId : "Not available"}</td>
                               </tr>
+                            )).slice(0, 8)}
                         </tbody>
                       </table>
+                      </>
                       <div className="flex justify-center">
                           <Link to={'/admin/clients'} className='text-center p-2 text-gray-500 hover:underline'>Show more</Link>
                       </div>
                       </>
-                      )
-                      ).slice(0, 8)
                       : (
                         <div className='rounded w-full h-full flex justify-center items-center my-3'>
                           <div>
