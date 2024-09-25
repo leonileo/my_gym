591075cd53da5a6d43c8cb59b192afa7d45e3a6b
leonileo <leolittleprogrammer@gmail.com>
2024-09-25 04:09:38 +0300

    Update: index.js

--- backend/index.js
+++ backend/index.js
@@ -30,19 +30,19 @@ app.use('/apiv1/trainer', trainerRoutes);
 app.use('/apiv1/client', clientRoutes);
 app.use('/apiv1/upload', uploadRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//   );
+// } else {
   const __dirname = path.resolve();
   app.use('/backend/uploads', express.static(path.join(__dirname, '/backend/uploads')));
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
-}
+// }
 
 app.use(notFound);
 app.use(errorHandler);
