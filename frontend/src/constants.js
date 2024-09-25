a0de5fd3296c28f1f436db01bac65a382b8dc01b
Hana-Developer <hanatadesse2023@gmail.com>
2024-09-25 03:13:10 +0300

    Update: added the chat url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -5,3 +5,5 @@ export const AUTH_URL = '/apiv1/auth';
 export const ADMIN_URL = '/apiv1/admin';
 export const CLIENT_URL = '/apiv1/client';
 export const TRAINER_URL = '/apiv1/trainer';
+export const CHAT_URL = '/apiv1/chat';
+export const MESSAGE_URL = '/apiv1/message';
