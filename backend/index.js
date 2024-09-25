0671045b1543b4c3bf3306b6a5fa1724f4e817b3
leonileo <leolittleprogrammer@gmail.com>
2024-09-25 04:26:13 +0300

    Updated

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use('/apiv1/upload', uploadRoutes);
 //     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 //   );
 // } else {
-  const __dirname = path.resolve();
+  // const __dirname = path.resolve();
   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.get('/', (req, res) => {
     res.send('API is running....');
