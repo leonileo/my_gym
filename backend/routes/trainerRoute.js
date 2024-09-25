f63bf3dd9ea89cdfd766f0ef48c709dc4109694a
Hana-Developer <hanatadesse2023@gmail.com>
2024-09-25 03:02:37 +0300

    Update: removed the chat routes

--- backend/routes/trainerRoute.js
+++ backend/routes/trainerRoute.js
@@ -3,7 +3,7 @@
 // Import necessary modules
 const express = require('express');
 const { protectTrainer, trainer } = require('../middleware/authMiddleware.js');
-const { dashboard, getClients, getService, getWorkout, createWorkout, getClientRequest, updateClientRequest, updateWorkout, deleteWorkout, getChats, getSpecificChat, addChat, getProfile, updateProfile, createService, updateService, deleteService, getClientProgress, assignWorkout, removeAssignedWorkout } = require('../controllers/trainerController.js')
+const { dashboard, getClients, getService, getWorkout, createWorkout, updateClientRequest, updateWorkout, deleteWorkout, getProfile, updateProfile, createService, updateService, deleteService, getClientProgress, assignWorkout, removeAssignedWorkout } = require('../controllers/trainerController.js')
 
 const router = express.Router();
 
@@ -20,25 +20,19 @@ router.route('/workout')
 .get( protectTrainer, trainer, getWorkout)
 .post( protectTrainer, trainer, createWorkout)
 
-router.route('/client-request')
-.get( protectTrainer, trainer, getClientRequest)
+.get( protectTrainer, trainer)
 router.route('/client-progress/')
 .post( protectTrainer, trainer, getClientProgress)
 router.route('/assign-workout')
 .put( protectTrainer, trainer, assignWorkout)
 router.route('/remove-assigned-workout')
 .put( protectTrainer, trainer, removeAssignedWorkout)
-router.route('/client-request/:id')
-.put( protectTrainer, trainer, updateClientRequest)
+router.put('/client-request', protectTrainer, trainer, updateClientRequest)
 
 router.route('/workout/:id')
 .put( protectTrainer, trainer, updateWorkout)
 .delete( protectTrainer, trainer, deleteWorkout)
 
-router.get('/chat',  protectTrainer, trainer, getChats)
-router.route('/chat/:id')
-.get( protectTrainer, trainer, getSpecificChat)
-.post( protectTrainer, trainer, addChat)
 router.route('/profile')
 .get( protectTrainer, trainer, getProfile)
 .put( protectTrainer, trainer, updateProfile)
