476e968046c17417f0942f0364b5f3d1d677ea6c
leonileo <leolittleprogrammer@gmail.com>
2024-09-26 08:39:39 +0300

    Update: added status url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,9 +1,10 @@
 // constants.js
 // API URL'S
 export const BASE_URL = '';
-export const AUTH_URL = 'https://my-gym-backend.onrender.com/apiv1/auth';
-export const ADMIN_URL = 'https://my-gym-backend.onrender.com/apiv1/admin';
-export const CLIENT_URL = 'https://my-gym-backend.onrender.com/apiv1/client';
-export const TRAINER_URL = 'https://my-gym-backend.onrender.com/apiv1/trainer';
-export const CHAT_URL = 'https://my-gym-backend.onrender.com/apiv1/chat';
-export const MESSAGE_URL = 'https://my-gym-backend.onrender.com/apiv1/message';
+export const STATUS_URL = '/apiv1/status';
+export const AUTH_URL = '/apiv1/auth';
+export const ADMIN_URL = '/apiv1/admin';
+export const CLIENT_URL = '/apiv1/client';
+export const TRAINER_URL = '/apiv1/trainer';
+export const CHAT_URL = '/apiv1/chat';
+export const MESSAGE_URL = '/apiv1/message';
