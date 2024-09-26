cddea23b5c9161f90cbe82f21d5eced70e72d225
leonileo <leolittleprogrammer@gmail.com>
2024-09-26 08:03:23 +0300

    Update: updated index.js

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,13 @@ const path = require('path');
 const dotenv = require('dotenv').config();
 const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db.js')
-const cors = require('cors');
 
 const authRoutes = require('./routes/authRoute.js')
 const adminRoutes = require('./routes/adminRoute.js')
 const trainerRoutes = require('./routes/trainerRoute.js')
 const clientRoutes = require('./routes/clientRoute.js')
 const uploadRoutes = require('./routes/uploadRoutes.js')
+const statusRoute = require('./routes/statusRoute.js')
 const { notFound, errorHandler } = require('./middleware/errorMiddleware.js')
 
 const port = process.env.PORT || 5000
@@ -20,30 +20,7 @@ connectDB()
 // initialize app
 const app = express()
 
-// cors
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
-
-app.use((req, res, next) => {
-res.setHeader("Access-Control-Allow-Origin", "https://my-gym.onrender.com/");
-res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-next();
-})
-
-app.options("/", (req, res) => {
-res.setHeader("Access-Control-Allow-Origin", "https://my-gym.onrender.com/");
-res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-res.sendStatus(204);
-});
-
 // main function
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -53,20 +30,25 @@ app.use('/apiv1/admin', adminRoutes);
 app.use('/apiv1/trainer', trainerRoutes);
 app.use('/apiv1/client', clientRoutes);
 app.use('/apiv1/upload', uploadRoutes);
+app.use('/apiv1/status', statusRoute);
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/build')));
+const ___dirname = path.resolve();
+app.use('/uploads', express.static(path.join(___dirname,'/uploads')));
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//   );
-// } else {
-  // const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(___dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(___dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const ___dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(___dirname, '/uploads')));
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
-// }
+}
 
 app.use(notFound);
 app.use(errorHandler);
