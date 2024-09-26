6506cbf88f4d912f887abbe260f023de22c7c2c0
leonileo <leolittleprogrammer@gmail.com>
2024-09-26 09:12:15 +0300

    Update: added the status api call on the homepage

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import HeaderComponent from '../components/HeaderComponent';
 import HeroComponent from '../components/HeroComponent';
 import TrainersComponent from '../components/TrainersComponent';
 import FeaturesComponent from '../components/FeaturesComponent';
 import CTAComponent from '../components/CTAComponent';
 import FooterComponent from '../components/FooterComponent';
-
+import { useStatusQuery } from '../slices/statusApiSlice';
 
 const HomeScreen = () => {
+  const { data, isLoading, error } = useStatusQuery()
 
   return (
     <div>
@@ -15,7 +16,7 @@ const HomeScreen = () => {
       <HeroComponent />
       <TrainersComponent />
       <FeaturesComponent />
-      <CTAComponent />
+      <CTAComponent isLoading={isLoading} data={data} error={error} />
       <FooterComponent />
     </div>
   )
