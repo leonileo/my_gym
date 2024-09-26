5f2af8af39e6e993bdc1d39e4f557a1c65aa0598
leonileo <leolittleprogrammer@gmail.com>
2024-09-26 14:20:14 +0300

    Update: updated the destination path

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, 'uploads/');
+        cb(null, 'backend/uploads/');
     },
     filename(req, file, cb) {
         cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
