b5015886ba9426fbfc36935921a4a5e8df84877d
leonileo <leolittleprogrammer@gmail.com>
2024-09-27 21:28:47 +0300

    Update: update the contents in the trainer component.

--- frontend/src/components/TrainersComponent.jsx
+++ frontend/src/components/TrainersComponent.jsx
@@ -4,17 +4,24 @@ import 'swiper/css/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
 import { motion } from 'framer-motion'
-import gymImg from '../assets/images/gymImg.jpg'
+import ma_pr from '../assets/images/ma_pr.png'
+import ma_req from '../assets/images/ma_req.png'
+import chk_da from '../assets/images/chk_da.png'
+import cr_wrkt from '../assets/images/cr_wrkt.png'
+import asgn_wrkt from '../assets/images/asgn_wrkt.png'
+import profile from '../assets/images/profile.png'
+import ad_service from '../assets/images/ad_service.png'
+import dashboard from '../assets/images/dashboard.png'
 
 const cards = [
-  {title: "Manage clients and their progress", img: gymImg},
-  {title: "Create personalized workout plans", img: gymImg},
-  {title: "Streamline your administrative tasks", img: gymImg},
-  {title: "Manage clients and their progress", img: gymImg},
-  {title: "Create personalized workout plans", img: gymImg},
-  {title: "Streamline your administrative tasks", img: gymImg},
-  {title: "Create personalized workout plans", img: gymImg},
-  {title: "Streamline your administrative tasks", img: gymImg},
+  {title: "Manage clients and their progress", img: ma_pr},
+  {title: "Get a personalized dashboard", img: dashboard},
+  {title: "Manage requests from clients", img: ma_req},
+  {title: "Check client data", img: chk_da},
+  {title: "Create personalized workout plans", img: cr_wrkt},
+  {title: "Assign workouts for client", img: asgn_wrkt},
+  {title: "Advertise services you provide", img: ad_service},
+  {title: "Update your profile", img: profile},
 ]
 
 export const FadeUp = (delay) => {
@@ -52,7 +59,7 @@ const TrainersComponent = () => {
       </div>
 
       <div className="swiperr w-full flex justify-end">
-
+        {/* for descktop */}
         <div className="md:w-[80%] w-full px-3 justify-end md:flex hidden ">
           <Swiper
             slidesPerView={3}
@@ -76,10 +83,10 @@ const TrainersComponent = () => {
                 style={{
                   backgroundImage: `linear-gradient(rgba(255, 255, 255, .1), rgba(0, 0, 0, .8)), url(${card.img})`,
                   backgroundRepeat: "no-repeat",
-                  backgroundSize: "cover",
+                  backgroundSize: "contain",
                   backgroundPosition: "center",
                 }}
-                className='mb-10 cursor-grab rounded overflow-hidden h-[90%] relative w-full bg-gradient-to-t from-[rgba(0,0,0,0.4)] to-white'>
+                className='transition-all hover:bg-transparent!important mb-10 cursor-grab rounded-md overflow-hidden h-[90%] relative w-full bg-gradient-to-t from-[rgba(0,0,0,0.4)] to-white'>
                   <div className="flex p-2 absolute bottom-0 justify-center w-full">
                     <p className='w-fit text-white text-xl text-center p-2 rounded capitalize'>{card.title}</p>
                   </div>
@@ -88,11 +95,12 @@ const TrainersComponent = () => {
             ))}
           </Swiper>
         </div>
-        <div className="md:w-[80%] w-full px-3 mb-5 justify-end md:hidden flex ">
+
+        {/* for small screens */}
+        <div className="md:w-[80%] w-full px-3 justify-end md:hidden flex ">
           <Swiper
             slidesPerView={2}
             spaceBetween={5}
-            centeredSlides={true}
             loop={true}
             pagination={{
               clickable: true,
@@ -111,14 +119,14 @@ const TrainersComponent = () => {
                 style={{
                   backgroundImage: `linear-gradient(rgba(255, 255, 255, .1), rgba(0, 0, 0, .8)), url(${card.img})`,
                   backgroundRepeat: "no-repeat",
-                  backgroundSize: "cover",
+                  backgroundSize: "contain",
                   backgroundPosition: "center",
                 }}
-                className='mb-10 cursor-grab rounded overflow-hidden h-[90%] relative w-full bg-gradient-to-t from-[rgba(0,0,0,0.4)] to-white'>
+                className='transition-all hover:bg-transparent!important mb-10 cursor-grab rounded-md overflow-hidden h-[90%] relative w-full bg-gradient-to-t from-[rgba(0,0,0,0.4)] to-white'>
                   <div className="flex p-2 absolute bottom-0 justify-center w-full">
-                    <p className='w-fit text-white text-center p-2 rounded capitalize'>{card.title}</p>
+                    <p className='w-fit line-clamp-1 hover:line-clamp-none transition-all text-pretty text-white md:text-xl text-center p-2 rounded capitalize'>{card.title}</p>
                   </div>
-              </div>
+              </div> 
               </SwiperSlide>
             ))}
           </Swiper>
